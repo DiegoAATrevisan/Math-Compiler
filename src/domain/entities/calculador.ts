ced1c8bcfc0086424873b2f0589ba8a9d0b7efcd
Diego Augusto Antonioli Trevisan <augustodiego02@gmail.com>
2023-12-08 17:59:50 -0300

    Relocating files to the correct directories.

---
+++ src/domain/entities/calculador.ts
@@ -0,0 +1,6 @@
+class calculador implements Idvisao{
+    divide(num1: number, num2: number): number {
+        throw new Error("Method not implemented.");
+    }
+
+}
---
+++ src/domain/interfaces/Idivisao.ts
@@ -0,0 +1,4 @@
+interface Idvisao {
+    divide(num1: number, num2: number): number;
+
+}
