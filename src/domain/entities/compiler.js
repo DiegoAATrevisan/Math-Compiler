d5298b00181827def994e33b5448b196b4b01d8a
Diego Augusto Antonioli Trevisan <augustodiego02@gmail.com>
2023-12-11 15:56:21 -0300

    Changed the calculation to "2+2" and added a function to validate if a character is a number.

--- src/domain/entities/compiler.js
+++ src/domain/entities/compiler.js
@@ -1,6 +1,19 @@
 function compile(calculo) {
     const converterParaInteiro = require('./usecases/conversion/Convert.js');
+    const validarSeContemNumero = require('./usecases/conversion/Validator.js')
     calculo = (calculo.split(""));
-    let contador = 0;
-
+    calculo = converterParaInteiro(calculo);
+    let num1;
+    let num2;
+    let operador;
+    for (const iterator of calculo) {
+        if (validarSeContemNumero(iterator) && num1 == "") {
+            num1 = iterator;
+        } else if (validarSeContemNumero(iterator) && num2 != "") {
+            num2 = iterator;
+        } else {
+            operador = iterator;
+        }
+    }
+    
 }
--- src/presentation/InAndOutPut.js
+++ src/presentation/InAndOutPut.js
@@ -1,5 +1,5 @@
 const compile = require('./entities/Compiler.js');
 
-var calcula = compile("2+2-2/2*2"); // Colocar calculo aqui
+var calcula = compile("2+2"); // Colocar calculo aqui
 
 console.log(calcula);
