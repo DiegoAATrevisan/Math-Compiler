d9841275531e5acde604e2581ae272da40413bf1
Diego Augusto Antonioli Trevisan <augustodiego02@gmail.com>
2023-12-18 18:10:07 -0300

    Add start script and fix compiler logic.

--- src/domain/entities/compiler.mjs
+++ src/domain/entities/compiler.mjs
@@ -4,13 +4,13 @@ import { execOperacao } from "../services/Calculator.mjs";
 export function compile(calculo) {
     calculo = (calculo.split(""));
     calculo = converterParaInteiro(calculo);
-    let num1;
-    let num2;
+    let num1 = "default";
+    let num2 = "default";
     let operador;
     for (const iterator of calculo) {
-        if (validarSeContemNumero(iterator) && num1 == "") {
+        if (validarSeContemNumero(iterator) && num1 == "default") {
             num1 = iterator;
-        } else if (validarSeContemNumero(iterator) && num2 != "") {
+        } else if (validarSeContemNumero(iterator) && num2 == "default") {
             num2 = iterator;
         } else {
             operador = iterator;
